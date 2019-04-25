453064a6f74869726aa84ddfd9af70155087cff1
Ben Ali <Othmen.BenAli@snp-ag.com>
2019-04-25 16:06:30 +0200

    first commit

---
+++ Backend/src/main/java/snp/fullstack/SnpFullStack.java
@@ -0,0 +1,12 @@
+package snp.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpFullStack {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnpFullStack.class, args);
+    }
+}
